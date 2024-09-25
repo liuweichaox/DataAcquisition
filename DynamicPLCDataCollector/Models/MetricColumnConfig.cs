52e753043da1c583971e93d5f8097939f5b7ac0a
Liu Weichao <liuweichaox@gmail.com>
2024-09-25 11:52:15 +0800

    feat: init

--- DynamicPLCDataCollector/Models/MetricColumnConfig.cs
+++ DynamicPLCDataCollector/Models/MetricColumnConfig.cs
@@ -5,31 +5,22 @@
 /// </summary>
 public class MetricColumnConfig
 {
-    /// <summary>
-    /// 配置ID
-    /// </summary>
-    public int Id { get; set; }
-    
-    /// <summary>
-    /// 采集ID
-    /// </summary>
-    public int MetricId { get; set; }
-    
+
     /// <summary>
     /// 列名
     /// </summary>
     public string ColumnName { get; set; }
-    
+
     /// <summary>
     /// 数据地址
     /// </summary>
     public string DataAddress { get; set; }
-    
+
     /// <summary>
     /// 数据长度
     /// </summary>
     public ushort DataLength { get; set; }
-    
+
     /// <summary>
     /// 数据类型
     /// </summary>
