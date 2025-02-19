b2dcc3ddb0b7412b475b1211f0cf56ebaace0cac
Liu Weichao <liuweichaox@gmail.com>
2025-02-19 14:18:41 +0800

    Update DataPoint.cs

--- DataAcquisition/Models/DataPoint.cs
+++ DataAcquisition/Models/DataPoint.cs
@@ -6,13 +6,8 @@ namespace DataAcquisition.Models;
 /// <summary>
 /// 数据点
 /// </summary>
-public class DataPoint
+public class DataPoint(Dictionary<string, object> values)
 {
     public DateTime Timestamp => DateTime.Now;
-    public Dictionary<string,object> Values { get; set; }
-
-    public DataPoint(Dictionary<string, object> values)
-    {
-        Values = values;
-    }
+    public Dictionary<string,object> Values { get; set; } = values;
 }
