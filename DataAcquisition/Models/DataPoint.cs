39ea0f7c45978da45389c56f1cff9f42daf059e9
Liu Weichao <liuweichaox@gmail.com>
2025-02-19 15:00:40 +0800

    Update DataPoint.cs

--- DataAcquisition/Models/DataPoint.cs
+++ DataAcquisition/Models/DataPoint.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace DataAcquisition.Models;
 
@@ -11,27 +10,4 @@ public class DataPoint(Dictionary<string, object> values)
 {
     public DateTime Timestamp => DateTime.Now;
     public Dictionary<string,object> Values { get; set; } = values;
-    public override int GetHashCode()
-    {
-        // 使用字典键值对计算哈希值
-        unchecked
-        {
-            int hash = 17;
-            foreach (var kvp in Values.OrderBy(k => k.Key))
-            {
-                hash = hash * 31 + kvp.Key.GetHashCode();
-                hash = hash * 31 + (kvp.Value?.GetHashCode() ?? 0);
-            }
-            return hash;
-        }
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is DataPoint other)
-        {
-            return Values.OrderBy(k => k.Key).SequenceEqual(other.Values.OrderBy(k => k.Key));
-        }
-        return false;
-    }
 }
