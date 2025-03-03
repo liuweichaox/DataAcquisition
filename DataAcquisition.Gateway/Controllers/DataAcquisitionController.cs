51525b176ecb98eb2be0f69335d1a3a8da050278
Liu Weichao <liuweichaox@gmail.com>
2025-03-03 17:18:45 +0800

    feat: update code

--- DataAcquisition.Gateway/Controllers/DataAcquisitionController.cs
+++ DataAcquisition.Gateway/Controllers/DataAcquisitionController.cs
@@ -31,4 +31,15 @@ public class DataAcquisitionController(IDataAcquisitionService dataAcquisitionSe
         dataAcquisitionService.StopCollectionTasks();
         return Ok("停止数据采集任务");
     }
+    
+    /// <summary>
+    /// 获取 PLC 连接状态
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult GetPlcConnectionStatus()
+    {
+        var plcConnectionStatus = dataAcquisitionService.GetPlcConnectionStatus();
+        return Ok(plcConnectionStatus);
+    }
 }
